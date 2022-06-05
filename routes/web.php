844fc950a1c67d8f75f5f087033f2e4126057b54
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-06-06 01:41:04 +0600

    redise compete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Blog;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,63 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
+});
+
+Route::get('/blogs/{id}', function($id){
+    $cachedBlog = Redis::get('blog_' . $id);
+
+
+  if(isset($cachedBlog)) {
+      $blog = json_decode($cachedBlog, FALSE);
+
+      return response()->json([
+          'status_code' => 201,
+          'message' => 'Fetched from redis',
+          'data' => $blog,
+      ]);
+  }else {
+      $blog = Blog::find($id);
+      Redis::set('blog_' . $id, $blog);
+
+      return response()->json([
+          'status_code' => 201,
+          'message' => 'Fetched from database',
+          'data' => $blog,
+      ]);
+  }
+});
+// ### Update ####
+Route::post('/blogs/update/{id}', function(Request $request,$id){
+
+    $update = Blog::findOrFail($id)->update($request->all());
+
+  if($update) {
+
+      // Delete blog_$id from Redis
+      Redis::del('blog_' . $id);
+
+      $blog = Blog::find($id);
+      // Set a new key with the blog id
+      Redis::set('blog_' . $id, $blog);
+
+      return response()->json([
+          'status_code' => 201,
+          'message' => 'User updated',
+          'data' => $blog,
+      ]);
+  }
+});
+
+// #### Delete #####
+
+Route::delete('/blogs/delete/{id}', function($id){
+    Blog::findOrFail($id)->delete();
+    Redis::del('blog_' . $id);
+
+    return response()->json([
+        'status_code' => 201,
+        'message' => 'Blog deleted'
+    ]);
 });
